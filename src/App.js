90c20f5519634aa5ef08761176a6a6bd9a8863ea
JhoellOpeyemi <jhoellpelumi@gmail.com>
2021-12-18 14:38:49 +0100

    switched from BrowserRouter to HashRouter in react-router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Search from "./components/Search";
